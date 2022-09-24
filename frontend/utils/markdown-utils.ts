f4e87fcc2f3c9075db450ce22578ab5992d8ea0e
nek0meshi <nekmeshi@gmail.com>
2022-09-24 23:07:21 +0900

    parse markdown sample

--- frontend/utils/markdown-utils.ts
+++ frontend/utils/markdown-utils.ts
@@ -1,8 +1,18 @@
+import fs from 'fs'
 import { remark } from 'remark'
+import remarkGfm from 'remark-gfm'
 import html from 'remark-html'
 
 export async function convertToHtml(md: string) {
-  const result = await remark().use(html).process(md)
+  const result = await remark().use(html).use(remarkGfm).process(md)
 
   return result.toString()
+}
+
+export async function loadFile(filePath: string) {
+  const contents = await convertToHtml(
+    fs.readFileSync(filePath, { encoding: 'utf8' })
+  )
+
+  return { contents }
 }
