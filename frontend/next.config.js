d074566b52ab4b2903e83b58f94cc508f7f4118a
nek0meshi <nekmeshi@gmail.com>
2022-09-25 01:24:28 +0900

    export static files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  // ref. https://stackoverflow.com/questions/62866083/how-do-i-omit-the-html-extension-in-next-js
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
