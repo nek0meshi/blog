ea684bd11ebbb816997bf0abe89aca20a832d8e5
nek0meshi <nekmeshi@gmail.com>
2022-09-25 14:33:57 +0900

    modify index pages

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
+  const linkData = [
+    {
+      path: 'posts',
+      title: '記事一覧',
+    },
+  ]
+  const links = (
+    <ul>
+      {linkData.map(({ path, title }) => (
+        <li key={path}>
+          <Link href={`/${path}`}>{title}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+
   return (
-    <div>Hello World!</div>
+    <section>
+      <h1>ブログ</h1>
+      {links}
+    </section>
   )
 }
 
