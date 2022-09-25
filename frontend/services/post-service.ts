59963abfae0a0e3132b71fee694e949fcda174eb
nek0meshi <nekmeshi@gmail.com>
2022-09-25 19:16:12 +0900

    parse matters

--- frontend/services/post-service.ts
+++ frontend/services/post-service.ts
@@ -1,11 +1,26 @@
 import fs from 'fs'
 import path from 'path'
+import dayjs from '@/lib/dayjs'
 import * as markdownUtils from '@/utils/markdown-utils'
+
+export type PostMatter = {
+  title: string
+  date: string
+}
 
 export async function loadMarkdown(slug: string) {
   const fullPath = path.join(getPostDir(), `${slug}.md`)
 
-  return markdownUtils.loadFile(fullPath)
+  const rawContent = fs.readFileSync(fullPath, { encoding: 'utf8' })
+  const postData = await markdownUtils.loadFile<PostMatter>(rawContent)
+
+  return {
+    content: postData.content,
+    matter: {
+      title: postData.matter.title,
+      date: dayjs(postData.matter.date).format('YYYY-MM-DD'),
+    },
+  }
 }
 
 export function getSlugs() {
