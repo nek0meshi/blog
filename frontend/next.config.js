37709b93483ab67e81012baca3724026e1558b8c
nek0meshi <nekmeshi@gmail.com>
2023-12-10 23:11:56 +0900

    - github.ioでリンクや画像が正しく表示されるように修正

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,5 @@
+const basePath = process.env.BASE_PATH ? '/' + process.env.BASE_PATH : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,7 +9,11 @@ const nextConfig = {
   },
   // ref. https://stackoverflow.com/questions/62866083/how-do-i-omit-the-html-extension-in-next-js
   trailingSlash: true,
-  basePath: process.env.BASE_PATH ? '/' + process.env.BASE_PATH : '',
+  basePath: basePath,
+  assertPrefix: basePath,
+  publicRuntimeConfig: {
+    basePath,
+  },
 }
 
 module.exports = nextConfig
